3238c8587b9ce452249b659b1ed453a4bca61717
Martin Mladenovski <martin@simplymeasured.com>
2014-03-27 19:44:37 -0700

    Initial setup for GoApp Engine

--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
+// import (
+//     "code.google.com/p/google-api-go-client/calendar/v3"
+// )
+
+// func main() {
+//     args := os.Args
+
+//     var config = &oauth.Config{
+//         ClientId:     "", // from https://code.google.com/apis/console/
+//         ClientSecret: "", // from https://code.google.com/apis/console/
+//         Scope:        calendar.CalendarScope,
+//         AuthURL:      "https://accounts.google.com/o/oauth2/auth",
+//         TokenURL:     "https://accounts.google.com/o/oauth2/token",
+//     }
+// }
+
 import (
-    "code.google.com/p/google-api-go-client/calendar/v3"
+	"fmt"
+	"net/http"
 )
 
-func main() {
-    args := os.Args
+func init() {
+	http.HandleFunc("/", handler)
+}
 
-
-    var config = &oauth.Config{
-        ClientId:     "", // from https://code.google.com/apis/console/
-        ClientSecret: "", // from https://code.google.com/apis/console/
-        Scope:        calendar.CalendarScope,
-        AuthURL:      "https://accounts.google.com/o/oauth2/auth",
-        TokenURL:     "https://accounts.google.com/o/oauth2/token",
-    }
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, world!")
 }
